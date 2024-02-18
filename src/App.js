15e29b0f144ea5d47e56930b602e58937cb3e27d
yamagumo <131755296+yamagumo@users.noreply.github.com>
2024-02-18 13:12:24 +0000

    ええ感じ

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          This Site is Profile Site By Momoyama Tech Club
+          山雲
+
+        
+          
         </p>
+        <p>
+           ネー〇ュ焼肉おごれ
+        </p>
+
+        <img src="Fhga6mtVEAAzQ1C.jpg" alt="ええ感じ" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://twitter.com/Kumo_kaze1341"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My twitter
         </a>
       </header>
     </div>
